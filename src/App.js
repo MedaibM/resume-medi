1766d7cfff0eb6da649fd905898af3cf1a4bd53c
MedaibM <128952491+MedaibM@users.noreply.github.com>
2024-03-14 01:24:00 -0400

    Initial changes with firebase integration (#1)
    * resume - initial with firebase implemented
    
    * added firebase github actions integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import Typewriter from 'typewriter-effect';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="title">
+        <Typewriter
+          options={{
+            strings: ['MEDAIB GEBREHIWOT', 'Resume'],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </span>
     </div>
   );
 }
